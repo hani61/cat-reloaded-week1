e6bf98720d2b339e1c6b5ee1838b9c68f20caa8e
hani <haniknight2003@gmail.com>
2025-03-07 22:36:58 +0200

    added first question

---
+++ introduction-1-9.js
@@ -0,0 +1,3 @@
+// code 1 injects javascript code before html document page is loaded completed
+// code 2 injects javascript code in a window function that calls onload function which it waits for the page until it's loaded to inject the code inside the function
+// code 3 injects javascript code after the html document is loaded completely¬¬
